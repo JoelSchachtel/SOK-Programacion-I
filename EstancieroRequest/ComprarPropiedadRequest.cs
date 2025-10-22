b3770d783e284d85cbca17b263511463c2a4f8fa
Joel Schachtel <joelschachtel@hotmail.com>
2025-10-22 18:24:45 -0300

    Se agregan archivos necesarios para manejar las propiedades

---
+++ EstancieroRequest/ComprarPropiedadRequest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EstancieroRequest
+{
+    public class ComprarPropiedadRequest
+    {
+        public int DniJugador { get; set; }
+        public int PropiedadId { get; set; }
+        public decimal Precio { get; set; }
+        public bool PuedeComprar { get; set; }
+    }
+}
